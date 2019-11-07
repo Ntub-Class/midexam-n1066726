bbca3376844c9b91057d1f257496437c359e05b2
Zeu <n1066726@ntub.edu.tw>
2019-11-07 20:46:05 +0800

    'OK-2'

--- src/q2.ts
+++ src/q2.ts
@@ -1,5 +1,7 @@
 import * as QRcode from 'qrcode';
+import * as readlineSync from 'readline-sync';
 // 題目說明: 請使用套件readline-sync跟node-qrcode，輸入文字來產生QRCODE
 // 輸入說明: 請打上任意文字，並產生QRCODE
 // 輸出說明: 請輸出檔名為QRCODE.png圖片
-QRcode.toFile('./QRCODE.png','Hello');
+let userName = readlineSync.question('請輸入你的名字');
+QRcode.toFile('./QRCODE.png',userName);
